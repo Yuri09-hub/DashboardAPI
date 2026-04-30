d77cef1501c4a30c70af2b703dbc8e2420e35733
Yuri Rodrigues <yurihenriques2004@gmail.com>
2026-04-30 23:03:18 +0100

    update

--- data_file_validation.py
+++ data_file_validation.py
@@ -1,6 +1,11 @@
 from fastapi import HTTPException
 import pandas as pd
 import matplotlib as mpl
+from fastapi.responses import StreamingResponse
+from io import BytesIO
+
+class PNGStreamingResponse(StreamingResponse):
+    media_type = "image/png"
 
 def clean(df):
     df = df.dropna()
@@ -8,10 +13,12 @@ def clean(df):
 
 def file_dataframe(file):
     if file.endswith(".csv"):
-        file_df = pd.read_csv(file)
-        return file_df
+        file_df = pd.read_csv(file,low_memory=False)
+        return clean(file_df)
     elif file.endswith(".xlsx"):
         file_df = pd.read_excel(file)
-        return file_df
+        return clean(file_df)
     else:
         return HTTPException(status_code=400,detail="File type not supported")
+
+def
--- form_route.py
+++ form_route.py
@@ -1,22 +1,29 @@
-from fastapi import APIRouter, UploadFile, File, HTTPException
+from fastapi import APIRouter, UploadFile, File, HTTPException, Depends
+from sqlalchemy.orm import Session
 from dependencies import get_session, verify_token
+from models import User, form
 from pathlib import Path
+from data_file_validation import file_dataframe
+
 file_save = Path('file_saves')
 
 form_route = APIRouter(tags=["form"], prefix="/form")
 
 @form_route.post("/uploadfile")
-async def upload_file(file: UploadFile):
+async def upload_file(file: UploadFile, user: User = Depends(verify_token),
+                      session:Session = Depends(get_session)):
     try:
         file_path = file_save / file.filename
 
         content = await file.read()
 
         file_path.write_bytes(content)
+
+        #locale = form(file=str(file_path),user_id=user.id)
+
         return {"filename": file.filename,
                 "location": f"{file_save}/{file.filename}"
-                }
-
+        }
     except OSError as e:
         print(e)
         return HTTPException(status_code=400, detail="Error")
