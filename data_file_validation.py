8484419df7ce3fce458ee45d59fb440213f98e69
Yuri Rodrigues <yurihenriques2004@gmail.com>
2026-05-01 09:56:55 +0100

    update

--- data_file_validation.py
+++ data_file_validation.py
@@ -1,24 +1,49 @@
 from fastapi import HTTPException
 import pandas as pd
-import matplotlib as mpl
-from fastapi.responses import StreamingResponse
+import matplotlib.pyplot as plt
 from io import BytesIO
+from enum import Enum
+import openpyxl
 
-class PNGStreamingResponse(StreamingResponse):
-    media_type = "image/png"
+class charType(str, Enum):
+    plot = "plot"
+    bar = "bar"
+    scatter = "scatter"
+    line = "line"
+    hist = "hist"
+
+
+
 
 def clean(df):
     df = df.dropna()
     return df
 
-def file_dataframe(file):
-    if file.endswith(".csv"):
-        file_df = pd.read_csv(file,low_memory=False)
+def file_dataframe(file,y,x):
+    if file.filename.endswith(".csv"):
+        file_df = pd.read_csv(file.file,low_memory=False)
+        file_df = file_df[[y, x]]
         return clean(file_df)
-    elif file.endswith(".xlsx"):
-        file_df = pd.read_excel(file)
+    elif file.filename.endswith(".xlsx"):
+        file_df = pd.read_excel(file.file)
+        file_df = file_df[[y, x]]
         return clean(file_df)
     else:
-        return HTTPException(status_code=400,detail="File type not supported")
+        raise HTTPException(status_code=400,detail="File type not supported")
 
-def
+
+def chart_generator(file_df,chart_type,y,x):
+
+    file_df.columns = file_df.columns.str.strip()
+
+    if x not in file_df.columns and y not in file_df.columns:
+        raise HTTPException(status_code=400,detail="Column not found")
+
+    fig, ax = plt.subplots()
+    file_df.plot(kind=chart_type, y=y, x=x)
+
+    buf = BytesIO()
+    fig.savefig(buf, format='png')
+    plt.close(fig)
+    buf.seek(0)
+    return buf
--- form_route.py
+++ form_route.py
@@ -1,31 +1,38 @@
 from fastapi import APIRouter, UploadFile, File, HTTPException, Depends
+from fastapi.responses import StreamingResponse
 from sqlalchemy.orm import Session
 from dependencies import get_session, verify_token
 from models import User, form
 from pathlib import Path
-from data_file_validation import file_dataframe
+from data_file_validation import charType, chart_generator, file_dataframe
+
+
+class PNGStreamingResponse(StreamingResponse):
+    media_type = "image/png"
+
 
 file_save = Path('file_saves')
 
 form_route = APIRouter(tags=["form"], prefix="/form")
 
+
 @form_route.post("/uploadfile")
-async def upload_file(file: UploadFile, user: User = Depends(verify_token),
-                      session:Session = Depends(get_session)):
-    try:
+async def upload_file( x:str, y:str, response:charType, user: User = Depends(verify_token),
+                      session: Session = Depends(get_session),file: UploadFile = File(...)):
+
+    if file.filename.endswith(".csv") or file.filename.endswith(".xlsx"):
         file_path = file_save / file.filename
-
         content = await file.read()
-
         file_path.write_bytes(content)
 
-        #locale = form(file=str(file_path),user_id=user.id)
+        # save relative path file
+       # locale = form(file=str(file_path), user_id=user.id)
+        #session.add(locale)
+        #session.commit()
 
-        return {"filename": file.filename,
-                "location": f"{file_save}/{file.filename}"
-        }
-    except OSError as e:
-        print(e)
-        return HTTPException(status_code=400, detail="Error")
+        file_df = file_dataframe(file, y.title(), x.title())
+        buf = chart_generator(file_df, response.value,y,x)
+        return StreamingResponse(buf, media_type="image/png")
+    raise HTTPException(status_code=400, detail="Error3")
 
 
